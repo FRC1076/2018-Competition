967c97704b1d6890338cea9e5aefcf4477c98c45
Helena <helenasheinonen@gmail.com>
2018-01-13 15:20:31 -0500

    wrote basics of robot's plan

---
+++ RobotPlan.md
@@ -0,0 +1,8 @@
+
+
+Tank drive
+  -
+2 pistons for lifter wings
+  -
+grabber and elevator in front
+  - wheels to pull cubes in
---
+++ robot.py
@@ -0,0 +1,18 @@
+import wpilib
+
+
+class Robot(wpilib.IterativeRobot):
+    def robotInit(self):
+        pass
+
+    def teleopInit(self):
+        pass
+
+    def teleopPeriodic(self):
+        pass
+    
+    def autonomousInit(self):
+        pass
+
+    def autonomousPeriodic(self):
+        pass
