5ab29ad0516139107d5da55e34e6365733131feb
Unknown <21cainp@gmail.com>
2018-01-14 16:45:50 -0500

    Made the code cleaner
    removed unneccesary true and false statments, made indentation better

--- strategy/sim.py
+++ strategy/sim.py
@@ -8,7 +8,7 @@ def team_bs():
     global boost_cubesr
     global score_b, score_r
     global boost_cubesb
-    
+
     power_cubesb = randint(24, 36)
     scale_b = randint(1, power_cubesb)
     force_cubesr = 0
@@ -28,36 +28,35 @@ def team_bs():
     power_cubesb = power_cubesb - switch_r_b 
     if power_cubesb >= 3:
         force_cubes = randint(0, 3)
-        if force_cubes == 1 and switch_b_b is False:
-        
+        if force_cubes == 1 and not switch_b_b:
             score_b = score_b + 15    
             score_r = 10 + score_r
-        if force_cubes == 2 and scale_b is False and scale_r is True:
+        if force_cubes == 2 and not scale_b and scale_r:
             score_b = score_b + 20
             score_r = score_r - 10
-            if scale_r is False and scale_b is False:
+            if not scale_r and not scale_b:
                 score_b = score_b + 10
-        if force_cubes == 3 and switch_b_b or scale_b is False:
+        if force_cubes == 3 and switch_b_b or not scale_b:
             score_b = score_b + 15
-            if scale_b is False and scale_r is True:
+            if not scale_b and scale_r:
                 score_b = score_b + 10
                 score_r = score_r - 10
-                if scale_b is False and scale_r is False:
+                if not scale_b and not scale_r:
                     score_b = score_b + 10
-            if switch_b_b is False:
+            if not switch_b_b:
                 score_b = score_b + 10
         power_cubesb - force_cubes == power_cubesb
     if power_cubesb >= 3:
         boost_cubesb = randint(0, 3)
-        if boost_cubesb == 1 and switch_b_b is True:
+        if boost_cubesb == 1 and switch_b_b:
             score_b = score_b + 15
-        if boost_cubesb == 2 and scale_b is True:
+        if boost_cubesb == 2 and scale_b:
             score_b = score_b + 20
-        if boost_cubesb == 3 and switch_b_b or scale_b is True:
+        if boost_cubesb == 3 and switch_b_b or scale_b:
             score_b = score_b + 15
-            if scale_b is True:
+            if scale_b:
                 score_b = score_b + 10 
-            if switch_b_b is True:
+            if switch_b_b:
                 score_b = score_b + 10
         power_cubesb = power_cubesb - boost_cubesb
     robo1_blue = randint(1,3)
@@ -98,35 +97,35 @@ def team_bs():
     switch_b_r = randint(0, power_cubesr)
     if power_cubesr >= 3:
         force_cubesr = randint(0, 3)
-        if force_cubesr == 1 and switch_r_r is False:
+        if force_cubesr == 1 and not switch_r_r:
             score_r = score_r + 15
             score_b = score_b - 10
-        if force_cubesr == 2 and scale_r is False and scale_b is True:
+        if force_cubesr == 2 and not scale_r and scale_b:
             score_r = score_r + 20
             score_b = score_b - 10
-            if scale_r is False and scale_b is False:
+            if not scale_r and not scale_b:
                 score_r = score_r + 20
-        if force_cubesr == 3 and switch_r_r or scale_r is False:
+        if force_cubesr == 3 and switch_r_r or not scale_r:
             score_r = score_r + 15
-            if scale_r is False and scale_b is True:
+            if not scale_r and scale_b:
                 score_r = score_r + 10
                 score_b = score_b - 10
-            if scale_b is False and scale_r is False:
+            if not scale_b and not scale_r:
                 score_r = score_r + 10
-            if switch_r_r is False:
+            if not switch_r_r:
                 score_r = score_r + 10
         power_cubesr = power_cubesr - force_cubesr
     if power_cubesr >= 3:
         boost_cubesr = randint(0, 3)
-        if boost_cubesr == 1 and switch_r_r is True:
+        if boost_cubesr == 1 and switch_r_r:
             score_r = score_r + 15
-        if boost_cubesr == 2 and scale_r is True:
+        if boost_cubesr == 2 and scale_r:
             score_r = score_r + 20
-        if boost_cubesr == 3 and switch_r_r or scale_r is True:
+        if boost_cubesr == 3 and switch_r_r or scale_r:
             score_r = score_r + 15
-            if scale_r is True:
+            if scale_r:
                 score_r = score_r + 10
-            if switch_r_r is True:
+            if switch_r_r:
                 score_r = score_r + 10
         power_cubesr = power_cubesr - boost_cubesr
     robo1_red = randint(1,3)
@@ -151,39 +150,34 @@ def team_bs():
         score_r = score_r + 90
         rpr = rpr + 1
     if zr == 1 and power_cubesr >= 3:
-     score_r = score_r + 75
-     power_cubesr = power_cubesr - 3
+        score_r = score_r + 75
+        power_cubesr = power_cubesr - 3
     if zr == 1 and power_cubesr <= 3:
-     score_r = score_r + 30
+        score_r = score_r + 30
     if zr == 0 and power_cubesr >= 3:
         score_r = score_r + 45
         power_cubesr = power_cubesr - 3
     else:
         pass
+
     if scale_r > scale_b:
-        scale_r is True
-        scale_b is False 
         score_r = score_r + 165
     elif scale_b > scale_r:
-        scale_b is True
-        scale_r is False
         score_b = score_b + 165
- 
+
     if switch_r_r > switch_r_b:
         score_r = score_r + 135
-        switch_r_r is True
-        switch_r_b is False
     elif switch_r_b > switch_r_r:
-        switch_r_r is False
+        pass
+
     if switch_b_b > switch_b_r:
         score_b = score_b + 135
-        switch_b_b is True
-        switch_b_r is False
     elif switch_b_r > switch_b_b:
-        switch_b_b is False
+        pass
+
     if score_b > score_r:
         rpb = rpb + 2
-    if score_r > score_b:
+    elif score_r > score_b:
         rpr = rpr + 2
     else:
         rpb = rpb + 1
