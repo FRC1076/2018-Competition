8d24bcd457b8f9fcf735f5e3cdfa00c569016d73
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-01-15 19:50:15 -0500

    Working 2018 Robotpy code!

--- robot.py
+++ robot.py
@@ -1,16 +1,16 @@
 import wpilib
-# import ctre
+import ctre
 import wpilib.drive
 
 class Robot(wpilib.IterativeRobot):
     def robotInit(self):
-        # self.l_motor = ctre.WPI_TalonSRX(3)
-        # self.l_motor2 = ctre.WPI_TalonSRX(4)
-        # self.r_motor = ctre.WPI_TalonSRX(1)
-        # self.r_motor2 = ctre.WPI_TalonSRX(2)
-        # self.left = wpilib.SpeedControllerGroup(self.l_motor, self.l_motor2)
-        # self.right = wpilib.SpeedControllerGroup(self.r_motor, self.r_motor2)
-        # self.robot_drive = wpilib.drive.DifferentialDrive(self.left, self.right)
+        self.l_motor = ctre.WPI_TalonSRX(3)
+        self.l_motor2 = ctre.WPI_TalonSRX(4)
+        self.r_motor = ctre.WPI_TalonSRX(1)
+        self.r_motor2 = ctre.WPI_TalonSRX(2)
+        self.left = wpilib.SpeedControllerGroup(self.l_motor, self.l_motor2)
+        self.right = wpilib.SpeedControllerGroup(self.r_motor, self.r_motor2)
+        self.robot_drive = wpilib.drive.DifferentialDrive(self.left, self.right)
         self.stick = wpilib.Joystick(0)
         # self.encoder = wpilib.Encoder(3, 4)
         pass
@@ -19,8 +19,9 @@ class Robot(wpilib.IterativeRobot):
         print("Teleop Init")
 
     def teleopPeriodic(self):
-        print(self.stick.getX())
-        # self.robot_drive.arcadeDrive(self.stick.getX(), self.stick.getY())
+        # print("l_motor speed" + str(self.l_motor.get()))
+        self.robot_drive.arcadeDrive(self.stick.getY(), self.stick.getX())
+        # self.l_motor.set(self.stick.getX())
         # print(self.l_motor.getQuadraturePosition() + " quad")
         # if self.r_motor.getEncVelocity() != 0:
         #     print("ENC1: " + str(self.r_motor.getEncVelocity()))
