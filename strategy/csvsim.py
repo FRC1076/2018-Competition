4e2f26c769be32f324b1648e6bfd22bb8260a04f
Unknown <21cainp@gmail.com>
2018-01-20 16:11:58 -0500

    Added all the information from the game simulation into the csv file!

--- strategy/csvsim.py
+++ strategy/csvsim.py
@@ -2,11 +2,31 @@ import sim
 import csv
 
 
-COLS = ['match_num', 'blue_score', 'red_score']
+COLS = [
+    'match_num',
+    'blue_score', 'red_score',
+    'blue_scale', 'red_scale',
+    'blue_own_switch', 'red_own_switch',
+    'blue_other_switch', 'red_other_switch',
+    'blue_boost_cubes', 'red_boost_cubes',
+    'blue_force_cubes', 'red_force_cubes',
+    'blue_climb_count', 'red_climb_count',
+    'blue_rp', 'red_rp',
+]
 
 
 def make_row(run, blue, red):
-    return [run, blue['score'], red['score']]
+    return [
+        run, 
+        blue['score'], red['score'], 
+        blue['scale'], red['scale'],
+        blue['own_switch'], red['own_switch'],
+        blue['other_switch'], red['other_switch'],
+        blue['boost_cubes'], red['boost_cubes'],
+        blue['force_cubes'], red['force_cubes'],
+        blue['climb_count'], red['climb_count'],
+        blue['rp'], red['rp'],
+    ]
 
 
 with open('stats.csv', 'w', newline='') as csvfile:
