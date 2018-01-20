614adef75049ef512e3fd6e43ad6403923fed31c
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-01-19 19:23:29 -0500

    Make a drivetrain in robot.py

--- robot.py
+++ robot.py
@@ -1,15 +1,39 @@
+import ctre
 import wpilib
+from wpilib.interfaces import GenericHID
 
+from subsystems.drivetrain import Drivetrain
+
+LEFT_STICK = GenericHID.Hand.kLeft
+RIGHT_STICK = GenericHID.Hand.kRight
+
+
+LEFT1_ID = 3
+LEFT2_ID = 4
+RIGHT1_ID = 1
+RIGHT2_ID = 2
 
 class Robot(wpilib.IterativeRobot):
     def robotInit(self):
-        pass
+        left1 = ctre.WPI_TalonSRX(LEFT1_ID)
+        left2 = ctre.WPI_TalonSRX(LEFT2_ID)
+        left = wpilib.SpeedControllerGroup(left1, left2)
+        right1 = ctre.WPI_TalonSRX(RIGHT1_ID)
+        right2 = ctre.WPI_TalonSRX(RIGHT2_ID)
+        right = wpilib.SpeedControllerGroup(right1, right2)
+        self.drivetrain = Drivetrain(left, right, None)
+
+
+        self.driver = wpilib.XboxController(0)
+        self.operator = wpilib.XboxController(1)
 
     def teleopInit(self):
         pass
 
     def teleopPeriodic(self):
-        pass
+        forward = self.driver.getY(RIGHT_STICK)
+        rotate = self.driver.getX(LEFT_STICK)
+        self.drivetrain.arcade_drive(forward, rotate)
 
     def autonomousInit(self):
         pass
