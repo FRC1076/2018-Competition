5692e974ac82400ba0cc389feb9b1ebf1276e20f
Aaron Kofsky <a2aarontothe2@gmail.com>
2018-03-22 21:17:54 -0400

    center

--- robot.py
+++ robot.py
@@ -224,7 +224,7 @@ class Robot(wpilib.IterativeRobot):
         game_message = wpilib.DriverStation.getInstance().getGameSpecificMessage()
         switch_position = autonomous.get_game_specific_message(game_message)
 
-        robot_position = autonomous.Position.LEFT # TODO: have an actual way to set this outside of the program
+        robot_position = autonomous.Position.CENTER # TODO: have an actual way to set this outside of the program
 
         # alliance_side =  wpilib.DriverStation.getInstance().getAlliance()
 
